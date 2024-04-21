1b513555e4d5855817050599fff1810b1b62bcfb
Sergio <sergiogontardo@gmail.com>
2024-04-21 11:22:33 -0400

    adding functions tarea2.py

--- tarea2.py
+++ tarea2.py
@@ -1,4 +1,6 @@
 import numpy as np
+from random import randint
+from enum import Enum
 
 ## 1) Complejidad algoritmo sumatoria(n) = O(n) (Lineal)
 
@@ -6,11 +8,11 @@ import numpy as np
 
 ## 3) Complejidad algoritmo duplicar_elementos(array) = O(n) (Lineal)
 
-## 4) Complejidad algoritmo
+## 4) Complejidad algoritmo ordenacion(array) = O(n^2) (Cuadrática)
 
-## 5) Complejidad algoritmo
+## 5) Complejidad algoritmo permutaciones(lista) = O(n!) (Factorial)
 
-## 6) Complejidad algoritmo
+## 6) Complejidad algoritmo suma de n números = O(n) (Lineal)
 
 ## 7) Cree una función recursiva llamada cuenta_regresiva(n), la cual debe imprimir los números desde el n ingresado hasta cero.
 
@@ -66,10 +68,90 @@ print(resultado)
 
 arr = np.array([1, 2, 3, 4, 5])
 
-arr[::-1]
-print(arr)
+nuevo_arr = arr[::-1]
+print(nuevo_arr)
 
-invertido_arr = np.flip(arr)
+arr1 = np.array([1, 2, 3, 4, 5])
+invertido_arr = np.flip(arr1)
 print(invertido_arr)
 
 
+## 12) Cree un función en Python llamada suma_espejo(arr1, arr2) que reciba como parámetros dosarreglos de igual longitud y devuelva otro arreglo con la suma de los valores del primer índice del primer arreglo con el último índice del segundo arreglo, la suma del valor del segundo índice del primer arreglo con el penúltimo valor del segundo arreglo
+
+arr1 = [1, 2, 3]
+arr2 = [1, 2, 3]
+
+def suma_espejo(arr1, arr2):
+    suma = []
+    for i in range(len(arr1)):
+        suma.append(arr1[i] + arr2[-i - 1])
+    return suma
+
+resultado = suma_espejo(arr1, arr2)
+print(resultado)
+
+
+## 13) Cree una función llamada concatena_array(arr1, arr2), la cual va a recibir dos array de strings deigual longitud y va a dar como resultado un string con la concatenación de los strings de cada par de indices de los arreglos.
+
+arr1 = np.array(["la", "de", "pradera", "de"])
+arr2 = np.array(["casa", "la", "es", "madera"])
+
+def concatena_array(arr1, arr2):
+    resultado = ""
+    for i in range(len(arr1)):
+        resultado += arr1[i] + " " + arr2[i] + " "
+    return resultado
+
+resultado = concatena_array(arr1, arr2)
+print(resultado)
+
+
+## 14) Complete las funciones preguntas() y respuestas(), para que al ejecutar el programa principal indique si la respuesta fue correcta, incorrecta o si se dejo sin contestar. En las funciones DEBE usar los tipos Enum mostrados. La función preguntas() no recibe parámetros y debe devolver de forma aleatoria algunas de las preguntas del Enum Preguntas, la función respuesta(preg, resp) recibe dos parámetros, el primero es la pregunta que generó el sistema, la segunda la respuesta que ingresó el usuario, y debe devolver del Enum Respuesta, según sea el caso.
+
+
+class Respuestas(Enum):
+    CORRECTO = "Respuesta correcta"
+    INCORRECTO = "La respuesta es incorrecta"
+    SIN_RESPUESTA = "Sin responder"
+
+class Preguntas(Enum):
+    PRIMERA = "¿Capital de Venezuela?"
+    SEGUNDA = "¿Cuánto es 2 x 7"
+    TERCERA = "¿Dónde queda la UCAB?"
+
+def preguntas():
+    number = randint(1,3)
+    if number == 1:
+        return Preguntas.PRIMERA.value
+    elif number == 2:
+        return Preguntas.SEGUNDA.value
+    else:
+        return Preguntas.TERCERA.value
+
+def respuesta(preg, res):
+    if preg == Preguntas.PRIMERA.value and res == "Caracas":
+        return Respuestas.CORRECTO.value
+    elif preg == Preguntas.SEGUNDA.value and res == "14":
+        return Respuestas.CORRECTO.value
+    elif preg == Preguntas.TERCERA.value and res == "Montalban":
+        return Respuestas.CORRECTO.value
+    else:
+        return Respuestas.INCORRECTO.value
+
+# Programa principal
+if __name__ == "__main__":
+    while True:
+        pregunta = preguntas()
+        print(pregunta)
+        print()
+
+        resp = input("Indique su respuesta: ")
+
+        result = respuesta(pregunta, resp)
+        print(result)
+
+        continuar = input("Desea continuar [s/n]: ").lower()
+        if continuar == "n":
+            break
+
+    print("Hasta Luego")
