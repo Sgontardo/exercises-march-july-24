db9e97f966b7eaff7a5a9d00d0a9abb59a749496
Sergio <sergiogontardo@gmail.com>
2024-05-04 22:04:47 -0400

    enum exercise py

--- estructuras_datos/colas.cpp
+++ estructuras_datos/colas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 // Definición de la estructura de nodo
--- taller_1_algoritmos/problema_4.py
+++ taller_1_algoritmos/problema_4.py
@@ -1,5 +1,25 @@
 from enum import Enum
 
 class Gramaje(Enum):
-    GRUESO = 1
-    DELGADO = 2
+    MUYPESADO = 40
+    PESADO = 30
+    NORMAL = 20
+    LIVIANO = 10
+
+def obtener_costo(peso):
+    if peso >= 4.0:
+        return Gramaje.MUYPESADO.value, Gramaje(40).name
+    elif peso >= 2.5 and peso < 4.0:
+        return Gramaje.PESADO.value, Gramaje(30).name
+    elif peso >= 1.0 and peso < 2.5:
+        return Gramaje.NORMAL.value, Gramaje(20).name
+    else:
+        return Gramaje.LIVIANO.value, Gramaje(10).name
+
+
+peso = float(input("Ingrese el peso del paquete: "))
+if peso <= 0:
+    print("El peso del paquete no puede ser menor o igual a 0")
+else:
+    costo, gramaje = obtener_costo(peso)
+    print(f"El costo del envio es de {costo}, debido a que el paquete es de gramaje {gramaje}")
