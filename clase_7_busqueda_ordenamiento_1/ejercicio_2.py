3fb1bb4a1eae7b92dbd98a0efef54a82e4a056f3
Sergio <sergiogontardo@gmail.com>
2024-06-10 21:12:12 -0400

    ejercicios busqueda - ordenar

--- clase_7_busqueda_ordenamiento_1/ejercicio_2.py
+++ clase_7_busqueda_ordenamiento_1/ejercicio_2.py
@@ -3,6 +3,15 @@ Crea una función que encuentre todas las posiciones de un elemento objetivo
 en un arreglo de cadenas de caracteres y devuelva un arreglo de índices. 
 Si el elemento no se encuentra, debe devolver una lista vacía.
 
-	arreglo = ["manzana", "banana", "cereza", "manzana", "pera", "manzana"] 	
-	objetivo = "manzana"
+
 """
+
+arr = ["manzana", "banana", "cereza", "manzana", "pera", "manzana"]
+objetivo = "manzana"
+
+def busqueda_secuencial(arr, objetivo):
+	indices = []
+	for i in range(len(arr)):
+		if arr[i] == objetivo:
+			indices.append(i)
+	return indices
